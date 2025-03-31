2f26a2e1b44d64a4cae36b5b3c6a8523f53f6c65
Raghav Taneja <raghavtaneja2808@gmail.com>
2025-04-01 02:13:57 +0530

    npm fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,17 +13,13 @@ function App() {
     dispatch(actions.fetchUser())
   },[dispatch])
     const auth=useSelector((state)=>state.auth)
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 document.documentElement.classList.toggle(
   "dark",
   localStorage.theme === "dark" ||
     (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
 );
-// Whenever the user explicitly chooses light mode
 localStorage.theme = "light";
-// Whenever the user explicitly chooses dark mode
 localStorage.theme = "dark";
-// Whenever the user explicitly chooses to respect the OS preference
 localStorage.removeItem("theme");
   return (
     <>
